84144a403015316ebb83217d0c76896b25d2a24f
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-07 17:14:28 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,7 +1,7 @@
 class Pokemon
   attr_accessor :id, :name, :type, :db
 
-  def initialize(id:, name:, type:, db:
+  def initialize(id:, name:, type:, db:)
     @id = id
     @name = name
     @type = type
@@ -18,6 +18,6 @@ class Pokemon
   end
 
   def alter_hp()
-    
+
   end
 end
